edbcdb7d53057ae53daa0bd7d75802f017562e96
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-03-22 13:15:02 -0600

    lordddd

---
+++ work-jesus/src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">
+        Big Mouth! 
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
