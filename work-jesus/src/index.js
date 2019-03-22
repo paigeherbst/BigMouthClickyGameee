340ae3b4c0a82e3461a7889cff9a9688c08af196
Paige Herbst <PMoney@Paiges-MacBook-Pro.local>
2019-03-22 13:26:21 -0600

    bug

--- work-jesus/src/index.js
+++ work-jesus/src/index.js
@@ -1,21 +1,12 @@
-import React from "react";
-import NavMessage from "../NavMessage";
-import "./style.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./App";
+import './index.css';
+import serviceWorker from "./serviceWorker";
+ReactDOM.render(<App />, document.getElementById("root"));
+serviceWorker();
 
-function Nav(props) {
-  return (
-    <nav className="navbar">
-      <ul>
-        <li className="brand">
-          <a href="/">Clicky Game!!</a>
-        </li>
-        <NavMessage score={props.score} topScore={props.topScore} />
-        <li>
-          Score: {props.score} | Top Score: {props.topScore}
-        </li>
-      </ul>
-    </nav>
-  );
-}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
 
-export default Nav;
